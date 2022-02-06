0b3e65acf00a8312a73b6ca1cc724dc999cde750
LianaBG <77899830+LianaBG@users.noreply.github.com>
2022-02-06 11:50:01 -0700

    Adding remaining test cases but not actually tested or ran.

--- Assignment2-artifact/TestCode/src/org/jfree/data/test/DataUtilitiesCalculateColumnTotalArrayTest.java
+++ Assignment2-artifact/TestCode/src/org/jfree/data/test/DataUtilitiesCalculateColumnTotalArrayTest.java
@@ -6,7 +6,7 @@ import org.junit.*;
 
 public class DataUtilitiesCalculateColumnTotalArrayTest {
     
-    @Test
+    @Test // done
     void testCCTAValid () {
         Mockery mockingContext = new Mockery ();
         final Values2D values = mockingContext.mock(Values2D.class);
@@ -36,27 +36,182 @@ public class DataUtilitiesCalculateColumnTotalArrayTest {
 
         assertEquals (result, 18.7, 0);
     }
+    
+    @Test // not sure what the results of this test should be
+    void testCCTAOutOfBoundsColumn () {
+        Mockery mockingContext = new Mockery ();
+        final Values2D values = mockingContext.mock(Values2D.class);
+        mockingContext.checking (new Expectations () {
+            {
+                one(values).getColumnCount ();
+                will(returnValue(3));
+                // first row
+                one(values).getValue(0,0);
+                will(returnValue(13.2));
+                one(values).getValue(0,1);
+                will(returnValue(1.7));
+                one(values).getValue(0,2);
+                will(returnValue(2.7));
+                // second row
+                one(values).getValue(1,0);
+                will(returnValue(4));
+                one(values).getValue(1,1);
+                will(returnValue(17));
+                one(values).getValue(1,2);
+                will(returnValue(2.1));
+            }
+        });
 
+        int [] validRows = {0,1};
+        try {
+            double result = DataUtilities.calculateColumnTotal (values, 7, validRows);
+            assertEquals (result, 0, 0);
+        } catch (Exception e) {
+            System.out.println("Exception thrown.");
+            assertEquals(0,0,0);
+        }
+    }
+    
+    @Test // done
+    void testCCTABoundaryColumn () {
+        Mockery mockingContext = new Mockery ();
+        final Values2D values = mockingContext.mock(Values2D.class);
+        mockingContext.checking (new Expectations () {
+            {
+                one(values).getColumnCount ();
+                will(returnValue(3));
+                // first row
+                one(values).getValue(0,0);
+                will(returnValue(13.2));
+                one(values).getValue(0,1);
+                will(returnValue(1.7));
+                one(values).getValue(0,2);
+                will(returnValue(2.7));
+                // second row
+                one(values).getValue(1,0);
+                will(returnValue(4));
+                one(values).getValue(1,1);
+                will(returnValue(17));
+                one(values).getValue(1,2);
+                will(returnValue(2.1));
+            }
+        });
+
+        int [] validRows = {0,1};
+        double result = DataUtilities.calculateColumnTotal (values, 0, validRows);
+
+        assertEquals (result, 17.2, 0);
+    }
+    
+    @Test // not sure what this should output
+    void testCCTAExtraValidRows () {
+        Mockery mockingContext = new Mockery ();
+        final Values2D values = mockingContext.mock(Values2D.class);
+        mockingContext.checking (new Expectations () {
+            {
+                one(values).getColumnCount ();
+                will(returnValue(3));
+                // first row
+                one(values).getValue(0,0);
+                will(returnValue(13.2));
+                one(values).getValue(0,1);
+                will(returnValue(1.7));
+                one(values).getValue(0,2);
+                will(returnValue(2.7));
+                // second row
+                one(values).getValue(1,0);
+                will(returnValue(4));
+                one(values).getValue(1,1);
+                will(returnValue(17));
+                one(values).getValue(1,2);
+                will(returnValue(2.1));
+            }
+        });
+
+        int [] validRows = {0,4};
+        double result = DataUtilities.calculateColumnTotal (values, 1, validRows);
+
+        assertEquals (result, 1.7, 0);
+    }
+    
+    @Test // not sure what this should result in
+    void testCCTANullData () {
+        int [] validRows = {0,1};
+        
+        try {
+            double result = DataUtilities.calculateColumnTotal (null, 1, validRows);
+            assertEquals (result, 0, 0);
+        } catch (Exception e) {
+            System.out.println("Exception thrown.");
+            assertEquals(0,0,0);
+        }
+
+    }
+    
+    @Test // done
+    void testCCTAENegativeValidValues () {
+        Mockery mockingContext = new Mockery ();
+        final Values2D values = mockingContext.mock(Values2D.class);
+        mockingContext.checking (new Expectations () {
+            {
+                one(values).getColumnCount ();
+                will(returnValue(3));
+                // first row
+                one(values).getValue(0,0);
+                will(returnValue(-13.2));
+                one(values).getValue(0,1);
+                will(returnValue(-1.7));
+                one(values).getValue(0,2);
+                will(returnValue(2.7));
+                // second row
+                one(values).getValue(1,0);
+                will(returnValue(4));
+                one(values).getValue(1,1);
+                will(returnValue(17));
+                one(values).getValue(1,2);
+                will(returnValue(-2.1));
+            }
+        });
+
+        int [] validRows = {0,1};
+        double result = DataUtilities.calculateColumnTotal (values, 1, validRows);
+
+        assertEquals (result, 15.3, 0);
+    }
+    
+    @Test // might return 0, might return an exception
+    void testCCTAEmptyValidRows () {
+        Mockery mockingContext = new Mockery ();
+        final Values2D values = mockingContext.mock(Values2D.class);
+        mockingContext.checking (new Expectations () {
+            {
+                one(values).getColumnCount ();
+                will(returnValue(3));
+                // first row
+                one(values).getValue(0,0);
+                will(returnValue(13.2));
+                one(values).getValue(0,1);
+                will(returnValue(1.7));
+                one(values).getValue(0,2);
+                will(returnValue(2.7));
+                // second row
+                one(values).getValue(1,0);
+                will(returnValue(4));
+                one(values).getValue(1,1);
+                will(returnValue(17));
+                one(values).getValue(1,2);
+                will(returnValue(2.1));
+            }
+        });
+
+        int [] validRows = {};
+        try {
+            double result = DataUtilities.calculateColumnTotal (values, 1, validRows);
+            assertEquals (result, 0, 0);
+        } catch (Exception e) {
+            System.out.println("Exception thrown");
+            assertEquals (0, 0, 0); // send proper finish
+        }
+    }
+    
 }
-
-// @Test
-// public void calculateColumnTotalForTwoValues() {
-//     // setup
-//     Mockery mockingContext = new Mockery();
-//     final Values2D values = mockingContext.mock(Values2D.class);
-//     mockingContext.checking(new Expectations() {
-//         {
-//             one(values).getRowCount();
-//             will(returnValue(2));
-//             one(values).getValue(0, 0);
-//             will(returnValue(7.5));
-//             one(values).getValue(1, 0);
-//             will(returnValue(2.5));
-//         }
-//     });
-//     // exercise	
-//     double result = DataUtilities.calculateColumnTotal(values, 0);
-//     // verify
-//     assertEquals(result, 10.0, .000000001d);
-//     // tear-down: NONE in this test method
-// }
